214437a1fd740f82194f610e62c8e4fead3d1079
panchen66 <798724054@qq.com>
2019-03-06 18:01:21 +0800

    LinkedListDel

---
+++ src/main/java/com/panchen/myAlgorithm/LinkedListDel.java
@@ -0,0 +1,16 @@
+package com.panchen.myAlgorithm;
+
+/**
+ * 
+ * 在单链表和双链表中删除倒数第K个节点
+ * 
+ * 題目来源: 程序员代码面试指南 IT名企算法与数据结构题目最优解
+ * 
+ * @author panchen
+ *
+ */
+public class LinkedListDel {
+    
+    
+
+}
