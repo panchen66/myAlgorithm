c61a8741ae0ba6bf7a84a347023cdd94abf4de5d
panchen66 <798724054@qq.com>
2019-07-10 18:04:43 +0800

    biggestSubBST

--- src/main/java/com/panchen/myAlgorithm/binaryTree/BinaryTree.java
+++ src/main/java/com/panchen/myAlgorithm/binaryTree/BinaryTree.java
@@ -79,4 +79,42 @@ public class BinaryTree {
             System.out.println(head.getValue());
         }
     }
+
+
+    // 找到二叉树中的最大搜索二叉子树
+    // 二叉搜索树 :
+    // 或者是一棵空树，或者是具有下列性质的二叉树： 若它的左子树不空，则左子树上所有结点的值均小于它的根结点的值；
+    // 若它的右子树不空，则右子树上所有结点的值均大于它的根结点的值； 它的左、右子树也分别为二叉排序树。
+    private Node biggestSubBST(Node head) {
+        return posOrderForBiggestSubBST(head, new int[3]);
+    }
+
+    private Node posOrderForBiggestSubBST(Node head, int[] record) {
+        if (head == null) {
+            
+            return null;
+        }
+        int value = head.value;
+        Node left = head.left;
+        Node right = head.right;
+        Node lBST = posOrderForBiggestSubBST(left, record);
+        int lSize = record[0];
+        int lmin = record[1];
+        int lmax = record[2];
+
+        Node rBST = posOrderForBiggestSubBST(left, record);
+        int rSize = record[0];
+        int rmin = record[1];
+        int rmax = record[2];
+
+        record[1] = lmin >= value ? value : lmin;
+        record[2] = rmax >= value ? rmax : value;
+        if (left == lBST && right == rBST && lmax < value && value < rmin) {
+            record[0] = lSize + rSize + 1;
+            return head;
+        }
+        record[0] = lSize >= rSize ? lSize : rSize;
+        return lSize > rSize ? lBST : rBST;
+
+    }
 }
