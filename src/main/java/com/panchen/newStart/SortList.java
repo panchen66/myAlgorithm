168fcac2fbe3d7931e29bf78f87d8ec54410f4c3
panchen <panchen@longshine.com>
2020-08-07 18:54:25 +0800

    SortList

---
+++ src/main/java/com/panchen/newStart/SortList.java
@@ -0,0 +1,86 @@
+package com.panchen.newStart;
+
+/**
+ * @Description: 148. 排序链表
+ * <p>
+ * 在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: 4->2->1->3 输出: 1->2->3->4 示例 2:
+ * <p>
+ * 输入: -1->5->3->4->0 输出: -1->0->3->4->5
+ * <p>
+ * <p>
+ * nlogn 二分思想  归并排序  实现来自大胃王同学 注释很清晰
+ * @author: chenp
+ * @date: 2020/08/07 18:31
+ */
+public class SortList {
+
+    public class ListNode {
+
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode sortList(ListNode head) {
+        // 1、递归结束条件
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        // 2、找到链表中间节点并断开链表 & 递归下探
+        ListNode midNode = middleNode(head);
+        ListNode rightHead = midNode.next;
+        midNode.next = null;
+
+        ListNode left = sortList(head);
+        ListNode right = sortList(rightHead);
+
+        // 3、当前层业务操作（合并有序链表）
+        return mergeTwoLists(left, right);
+    }
+
+    //  找到链表中间节点（876. 链表的中间结点）
+    private ListNode middleNode(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    // 合并两个有序链表（21. 合并两个有序链表）
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode sentry = new ListNode(-1);
+        ListNode curr = sentry;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+
+            curr = curr.next;
+        }
+
+        curr.next = l1 != null ? l1 : l2;
+        return sentry.next;
+    }
+
+}
